a4778e370f69e19f0f88db8ef3f35f56a7663255
Dave Kraytsberg <dkraytsberg@gmail.com>
2021-06-19 15:36:38 -0400

    remove extra components

--- src/components/library/Section.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Section: React.FC = ({ children }) => {
-  return <section className="Section">{children}</section>;
-};
-
-export default Section;
--- src/components/library/index.tsx
+++
@@ -1,6 +0,0 @@
-import "./styles.scss";
-import AbilityText from "./AbilityText";
-import Section from "./Section";
-import Header from "./Header";
-
-export { AbilityText, Section, Header };
