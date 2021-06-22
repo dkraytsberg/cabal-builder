e07599320b8050e82117f36a940b5e3298cc613e
Dave Kraytsberg <dkraytsberg@gmail.com>
2021-06-21 21:18:15 -0400

    zen mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import DevoutCard from "./components/DevoutCard";
 import PhilosophySelect from "./components/PhilosophySelect";
 import LeaderSelect from "./components/LeaderSelect";
@@ -8,22 +9,31 @@ import PhilosophyInfo from "./components/PhilosophyInfo";
 import "./App.scss";
 
 function App() {
+  const [zenMode, setZenMode] = React.useState(false);
+
+  const toggleZenMode = () => setZenMode(!zenMode);
+
   return (
-    <div className="App">
-      <nav data-hide-on-print>
-        <h3>Leader</h3>
-        <LeaderSelect />
-        <h3>Philosophy</h3>
-        <PhilosophySelect />
-        <h3>Demons</h3>
-        <RosterPicker />
-      </nav>
-      <main>
-        <PhilosophyInfo />
-        <Leader />
-        <DevoutCard />
-        <Roster />
-      </main>
+    <div className={`App ${zenMode ? "zen-mode" : ""}`}>
+      <button onClick={toggleZenMode} className="ZenToggle">
+        {zenMode ? "exit zen mode" : "zen mode"}
+      </button>
+      <div className="App__Body">
+        <nav data-hide-on-print>
+          <h3>Leader</h3>
+          <LeaderSelect />
+          <h3>Philosophy</h3>
+          <PhilosophySelect />
+          <h3>Demons</h3>
+          <RosterPicker />
+        </nav>
+        <main>
+          <PhilosophyInfo />
+          <Leader />
+          <DevoutCard />
+          <Roster />
+        </main>
+      </div>
     </div>
   );
 }
