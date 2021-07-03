3c55e40486b54856be8b6f1e422398b04b4d51a1
Dave Kraytsberg <dkraytsberg@gmail.com>
2021-07-02 22:12:45 -0400

    free demon for lords of hell

--- src/components/PhilosophyInfo.tsx
+++ src/components/PhilosophyInfo.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { selectPhilosopy, selectRoster } from "../redux/selectors";
+import { DemonType, LordsOfHell } from "../types/DemonTypes";
 import Ability from "./Ability";
 import { MainHeader, InlineHeader } from "./lib/Typography";
 
@@ -8,15 +9,36 @@ const PhilosophyInfo: React.FC = () => {
 
   const roster = useSelector(selectRoster);
 
+  const { totalCost, mostExpensiveLesserDemon } = roster.reduce(
+    ({ totalCost, mostExpensiveLesserDemon }, demon) => {
+      const {
+        data: { cost, type },
+      } = demon;
+      return {
+        totalCost: cost + totalCost,
+        mostExpensiveLesserDemon:
+          cost > mostExpensiveLesserDemon && type === DemonType.Lesser
+            ? cost
+            : mostExpensiveLesserDemon,
+      };
+    },
+    { totalCost: 0, mostExpensiveLesserDemon: 0 }
+  );
+
+  const costDisplay =
+    name === LordsOfHell && totalCost > mostExpensiveLesserDemon
+      ? `Cost ${
+          totalCost - mostExpensiveLesserDemon
+        } (${totalCost} with free demon)`
+      : `Cost ${totalCost}`;
+
   return (
     <div className="PhilosophyInfo">
       <header>
         <MainHeader>{name}</MainHeader>
       </header>
       <article>
-        <InlineHeader>
-          Cost: {roster.reduce((total, demon) => demon.data.cost + total, 0)}
-        </InlineHeader>
+        <InlineHeader>{costDisplay}</InlineHeader>
         &nbsp;&nbsp;
         <InlineHeader>Cabal Size: {roster.length + 2} / 10</InlineHeader>
       </article>
